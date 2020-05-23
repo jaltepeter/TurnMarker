7c4565a9840c3f4a5fd4fdb4f5f91bd6d383b307
Jeremiah Altepeter <jeremiah@altepeter.com>
2020-05-23 14:34:54 -0700

    add support for hex grids

--- turnmarker/module/marker.js
+++ turnmarker/module/marker.js
@@ -19,13 +19,15 @@ export class Marker {
 
             let token = findTokenById(tokenId);
             let ratio = Settings.getRatio();
+            let dims = this.getImageDimensions(token);
+            let center = this.getImageLocation(token);
 
             let newTile = new Tile({
                 img: Settings.getImagePath(),
-                width: token.w * ratio,
-                height: token.h * ratio,
-                x: token.center.x - ((token.w * ratio) / 2),
-                y: token.center.y - ((token.h * ratio) / 2),
+                width: dims.w,
+                height: dims.h,
+                x: center.x,
+                y: center.y,
                 z: 900,
                 rotation: 0,
                 hidden: token.data.hidden,
@@ -50,13 +52,15 @@ export class Marker {
     static async moveMarkerToToken(tokenId, markerId) {
         let token = findTokenById(tokenId);
         let ratio = Settings.getRatio();
+        let dims = this.getImageDimensions(token);
+        let center = this.getImageLocation(token);
 
         await canvas.scene.updateEmbeddedEntity('Tile', {
             _id: markerId,
-            width: token.w * ratio,
-            height: token.h * ratio,
-            x: token.center.x - ((token.w * ratio) / 2),
-            y: token.center.y - ((token.h * ratio) / 2),
+            width: dims.w,
+            height: dims.h,
+            x: center.x,
+            y: center.y,
             hidden: token.data.hidden
         });
     }
@@ -74,6 +78,9 @@ export class Marker {
         }
     }
 
+    /**
+     * Updates the tile image when the image path has changed
+     */
     static updateImagePath() {
         if (game.user.isGM) {
             let tile = canvas.tiles.placeables.find(t => t.data.flags.turnMarker == true);
@@ -93,5 +100,56 @@ export class Marker {
     static reset(animator) {
         MarkerAnimation.stopAnimation(animator);
         this.clearAllMarkers();
+    }
+
+    /**
+     * Gets the proper dimensions of the marker tile taking into account the current grid layout
+     * @param {object} token - The token that the tile should be placed under
+     */
+    static getImageDimensions(token) {
+        let ratio = Settings.getRatio();
+        let newWidth = 0;
+        let newHeight = 0;
+
+        switch (canvas.grid.type) {
+            case 2: case 3: // Hex Rows
+                newWidth = newHeight = token.h * ratio;
+                break;
+            case 4: case 5: // Hex Columns
+                newWidth = newHeight = token.w * ratio;
+                break;
+            default: // Gridless and Square
+                newWidth = token.w * ratio;
+                newHeight = token.h * ratio;
+                break;
+        }
+
+        return { w: newWidth, h: newHeight };
+    }
+
+    /**
+     * Gets the proper location of the marker tile taking into account the current grid layout
+     * @param {object} token - The token that the tile should be placed under
+     */
+    static getImageLocation(token) {
+        let ratio = Settings.getRatio();
+        let newX = 0;
+        let newY = 0;
+
+        switch (canvas.grid.type) {
+            case 2: case 3: // Hex Rows
+                newX = token.center.x - ((token.h * ratio) / 2);
+                newY = token.center.y - ((token.h * ratio) / 2);
+                break;
+            case 4: case 5: // Hex Columns
+                newX = token.center.x - ((token.w * ratio) / 2);
+                newY = token.center.y - ((token.w * ratio) / 2);
+                break;
+            default: // Gridless and Square
+                newX = token.center.x - ((token.w * ratio) / 2);
+                newY = token.center.y - ((token.h * ratio) / 2);
+        }
+
+        return { x: newX, y: newY };
     }
 }
